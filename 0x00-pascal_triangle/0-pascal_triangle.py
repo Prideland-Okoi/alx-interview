41b8ff528c1b131e66d14f8f9e01365d38c3abd3
Prideland-Okoi <prideland.okoi@gmail.com>
2023-01-26 10:49:54 +0000

    pascal triangle solved

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -12,12 +12,12 @@ def pascal_triangle(n):
     if n == 1:
         return [[1]]
     if n == 2:
-        return [[1], [1,1]]
-    triangle = [[1], [1,1]]
+        return [[1], [1, 1]]
+    triangle = [[1], [1, 1]]
     for i in range(2, n):
         row = [1]
         for a in range(1, i):
-            row.append(triangle [i-1][a-1] + triangle[i-1][a] + triangle[i-1][a-1])
+            row.append(triangle[i-1][a-1] + triangle[i-1][a])
             row.append(1)
             triangle.append(row)
             return triangle
