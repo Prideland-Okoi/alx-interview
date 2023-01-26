e0f5e894deadd6fb5a59716900fa364e3bb1845b
Prideland-Okoi <prideland.okoi@gmail.com>
2023-01-26 23:26:55 +0000

    task solved

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,23 +1,35 @@
 #!/usr/bin/python3
-'''
-Create a function `def pascal_triangle(n):` that returns a list of lists of integers representing the Pascal’s triangle of
-'''
+"""Pascal's Triangle"""
+
 
 def pascal_triangle(n):
-    '''
-    create a list of lists of integers representing the Pascal's triangle of a given integer
-    '''
-    if type(n) is not int or n <= 0:
-        return([])
-    if n == 1:
-        return [[1]]
-    if n == 2:
-        return [[1], [1, 1]]
-    triangle = [[1], [1, 1]]
-    for i in range(2, n):
-        row = [1]
-        for a in range(1, i):
-            row.append(triangle[i-1][a-1] + triangle[i-1][a])
-            row.append(1)
-            triangle.append(row)
-            return triangle
+  """returns a list of lists of integers representing the Pascal’s triangle of n
+  Args:
+      n (n): size of triangle
+  Returns:
+      list: returns empty list if n <= 0 or list of lists of integers
+      representing Pascal's triangle of n otherwise
+  """
+  if n <= 0:
+    return []
+  
+  triangle, store_list = [], []
+  for a in range(n):
+    if a == 0:
+      triangle.append([1])
+      continue
+    if a == 1:
+      triangle.append([1, 1])
+      continue
+    
+    sum_figs_list = tri[-1]
+    for b in range(len(sum_figs_list) + 1):
+      if b in [0, len(sum_figs_list)]:
+        store_list.append(1)
+        continue
+      store_list.append(sum_figs_list[b] + sum_figs_list[b - 1])
+    
+    triangle.append(store_list)
+    store_list = []
+    
+  return triangle
