3aec8864d012b6914cce1b5e042d2d31c2c418b4
Prideland-Okoi <prideland.okoi@gmail.com>
2023-01-27 10:11:03 +0000

    pep8 applied to script

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -5,10 +5,10 @@
 def pascal_triangle(n):
     """returns a list of lists of integers repr the Pascal’s triangle of n
     Args:
-        n (n): size of triangle
+      n (n): size of triangle
     Returns:
-        list: returns empty list if n <= 0 or list of lists of integers
-        representing Pascal's triangle of n otherwise
+      list: returns empty list if n <= 0 or list of lists of integers
+      representing Pascal's triangle of n otherwise
     """
     if n <= 0:
         return []
@@ -16,16 +16,16 @@ def pascal_triangle(n):
     for a in range(n):
         if a == 0:
             triangle.append([1])
-        continue
+            continue
         if a == 1:
             triangle.append([1, 1])
-        continue
+            continue
         sum_figs_list = triangle[-1]
         for b in range(len(sum_figs_list) + 1):
             if b in [0, len(sum_figs_list)]:
                 store_list.append(1)
-        continue
-        store_list.append(sum_figs_list[b] + sum_figs_list[b - 1])
+                continue
+            store_list.append(sum_figs_list[b] + sum_figs_list[b - 1])
         triangle.append(store_list)
         store_list = []
     return triangle
