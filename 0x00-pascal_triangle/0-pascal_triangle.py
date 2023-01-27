41a05e01c3b79126edbf9f0e2fc6b017641fae0c
Prideland-Okoi <prideland.okoi@gmail.com>
2023-01-27 08:58:45 +0000

    pep8 applied to script

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -3,29 +3,29 @@
 
 
 def pascal_triangle(n):
-  """returns a list of lists of integers repr the Pascal’s triangle of n
-  Args:
-      n (n): size of triangle
-  Returns:
-      list: returns empty list if n <= 0 or list of lists of integers
-      representing Pascal's triangle of n otherwise
-  """
-  if n <= 0:
-    return []
-  triangle, store_list = [], []
-  for a in range(n):
-    if a == 0:
-      triangle.append([1])
-      continue
-    if a == 1:
-      triangle.append([1, 1])
-      continue
-    sum_figs_list = triangle[-1]
+    """returns a list of lists of integers rep the Pascal’s triangle of n
+    Args:
+        n (n): size of triangle
+    Returns:
+        list: returns empty list if n <= 0 or list of lists of integers
+        representing Pascal's triangle of n otherwise
+    """
+    if n <= 0:
+        return []
+    triangle, store_list = [], []
+    for a in range(n):
+        if a == 0:
+            triangle.append([1])
+            continue
+        if a == 1:
+            triangle.append([1, 1])
+            continue
+        sum_figs_list = triangle[-1]
     for b in range(len(sum_figs_list) + 1):
-      if b in [0, len(sum_figs_list)]:
-        store_list.append(1)
-        continue
-      store_list.append(sum_figs_list[b] + sum_figs_list[b - 1])
-    triangle.append(store_list)
-    store_list = []  
-  return triangle
+        if b in [0, len(sum_figs_list)]:
+            store_list.append(1)
+            continue
+        store_list.append(sum_figs_list[b] + sum_figs_list[b - 1])
+        triangle.append(store_list)
+        store_list = []
+    return triangle
