8adb12890235de5547ea5c70e35501e2cf7c9a31
Prideland-Okoi <prideland.okoi@gmail.com>
2023-01-27 09:26:40 +0000

    pep8 applied to script

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -11,22 +11,21 @@ def pascal_triangle(n):
         representing Pascal's triangle of n otherwise
     """
     if n <= 0:
-      return []
+        return []
     triangle, store_list = [], []
     for a in range(n):
-      if a == 0:
-        triangle.append([1])
+        if a == 0:
+            triangle.append([1])
         continue
-      if a == 1:
-        triangle.append([1, 1])
+        if a == 1:
+            triangle.append([1, 1])
         continue
-      sum_figs_list = triangle[-1]
-      for b in range(len(sum_figs_list) + 1):
-        if b in [0, len(sum_figs_list)]:
-          store_list.append(1)
-          continue
+        sum_figs_list = triangle[-1]
+        for b in range(len(sum_figs_list) + 1):
+            if b in [0, len(sum_figs_list)]:
+                store_list.append(1)
+        continue
         store_list.append(sum_figs_list[b] + sum_figs_list[b - 1])
-      triangle.append(store_list)
-      store_list = []
+        triangle.append(store_list)
+        store_list = []
     return triangle
-
